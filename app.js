1da7cb26b89045a9147199c79af0d3c752e2dafa
Ubuntu <ubuntu@ip-172-31-90-226.ec2.internal>
2019-07-08 14:20:13 +0000

    creating app

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+
+
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("Web app is up and running"); 
+});
