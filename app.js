e7de2b2c481981ef5423e30091dc866a6a96b175
Leo Garza <leog4za@gmail.com>
2023-08-11 00:57:05 -0500

    secure https

--- app.js
+++ app.js
@@ -64,15 +64,15 @@ app.use((req, res, next) => {
     next();
 });
 
-// app.use (function (req, res, next) {
-//     if (req.secure) {
-//             // request was via https, so do no special handling
-//             next();
-//     } else {
-//             // request was via http, so redirect to https
-//             res.redirect('https://' + req.headers.host + req.url);
-//     }
-// });
+app.use (function (req, res, next) {
+    if (req.secure) {
+            // request was via https, so do no special handling
+            next();
+    } else {
+            // request was via http, so redirect to https
+            res.redirect('https://' + req.headers.host + req.url);
+    }
+});
 
 app.use('/', viewRouter); // get view Routes
 
